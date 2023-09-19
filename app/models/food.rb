3a0c7c4e178fa0422b956d5d901d44fedb4d6b43
toyman640 <falakoadebayor@gmail.com>
2023-09-19 21:29:39 +0100

    Fix linters errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,11 +5,11 @@ class Food < ApplicationRecord
   validates :measurement_unit, presence: true
   validates :quantity, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-  
+
   before_validation :set_default_price
-  
+
   private
-  
+
   def set_default_price
     self.price ||= 0
   end
