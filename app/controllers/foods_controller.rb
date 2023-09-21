6e35ce86817ecb2f70ae99d2ba1211435be8068a
toyman640 <falakoadebayor@gmail.com>
2023-09-21 14:45:03 +0100

    Add test for foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,7 +3,7 @@ class FoodsController < ApplicationController
   before_action :set_food, only: %i[show destroy]
 
   def index
-    @foods = Food.all
+    @foods = current_user.foods
   end
 
   def create
@@ -22,7 +22,7 @@ class FoodsController < ApplicationController
 
   def destroy
     if @food.destroy
-      redirect_to foods_path, notice: 'Food item was successfully deleted.'
+      redirect_to foods_path, flash[:notice] = 'Food item was successfully deleted.'
     else
       flash.now[:alert] = 'Food item could not be deleted.'
       render :show
@@ -33,7 +33,7 @@ class FoodsController < ApplicationController
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 
-  private
+  private 
 
   def set_food
     @food = Food.find(params[:id])
