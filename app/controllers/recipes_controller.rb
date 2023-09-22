f2993066594d2066637282eee6238d1cf23ae437
David Katende <katendedavidjoga@gmail.com>
2023-09-22 17:56:46 +0300

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,4 @@
-require 'pry'
-
 class RecipesController < ApplicationController
-  # before_action :authenticate_user!, except: [:show]
-
   def index
     @recipes = current_user.recipes
   end
@@ -12,8 +8,8 @@ class RecipesController < ApplicationController
   end
 
   def add_ingredient
-     @recipe = Recipe.left_outer_joins(:recipe_foods).select('recipes.*,recipe_foods.quantity').find(params[:id])
-    binding.pry
+    @recipe = Recipe.left_outer_joins(:recipe_foods).select('recipes.*,recipe_foods.quantity').find(params[:id])
+
     if current_user == @recipe.user
       @foods_not_in_recipe = Food.where.not(id: @recipe.foods.pluck(:id))
 
@@ -22,7 +18,6 @@ class RecipesController < ApplicationController
         Food.where(id: food_ids).each do |food|
           RecipeFood.create!(food:, recipe: @recipe, quantity: params[:recipe][:quantity])
         end
-        # @recipe.foods << foods_to_add
         flash[:notice] = 'Ingredients added successfully.'
         redirect_to @recipe
         return
@@ -37,8 +32,6 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @user = @recipe.user
-    # p "recipe user:#{@user.id}"
-    # p "recipe user:#{current_user.id}"
     @recipe_food = @recipe.foods.joins(:recipe_foods).select('foods.*,recipe_foods.quantity').distinct
   end
 
